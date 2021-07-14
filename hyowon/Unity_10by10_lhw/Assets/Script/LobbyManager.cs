2ae2069c0b71f1863ac9125650eefd447a2e2c07
Recorder08 <dlgydnjs08@naver.com>
2021-07-14 18:16:07 +0900

    [Review] 0714 스터디 대비 리뷰 정리
    [Review] 0714 스터디 대비 리뷰 정리 및 bug fix

---
+++ hyowon/Unity_10by10_lhw/Assets/Script/LobbyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+
+  [SerializeField]
+  private GameObject Circle;
+  private float time;
+  private void Start()
+  {
+    time = 0;
+  }
+  private void Update()
+  {
+    time += Time.deltaTime;
+    if (time > 0.01f)
+    {
+      Circle.transform.Rotate(0, 0, 1f);
+      time = 0;
+    }
+  }
+  public void StartGame()
+  {
+    SceneManager.LoadScene("MainScene");
+  }
+}
