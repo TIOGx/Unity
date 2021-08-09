e70c3ec76ace86ffae9db6716ce7f84345d376e0
Recorder08 <dlgydnjs08@naver.com>
2021-08-09 17:16:06 +0900

    [Update] Chess
    Scripts DamageText = 피격 데미지(piece 의 자식인 Hud 위치에 생성됨) 표시
    
    각 피스의 상단에 체력바 표시 및 피격시 업데이트

--- hyowon/BattleChessProject/Assets/Scripts/PieceController.cs
+++ hyowon/BattleChessProject/Assets/Scripts/PieceController.cs
@@ -1,36 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PieceController : MonoBehaviour
 {
 
     public float Hp;
+    public float nowHp;
     public float OffensePower;
     public int Dir;
     public bool Attackable;
     private int[] dx = new int[]{0,1,0,-1};
     private int[] dy = new int[]{1,0,-1,0};
+    public GameObject hudDamageText;
+    public Transform Hudpos;
+    public GameObject HealthBar;
     void Start(){
         if(this.tag == "BlackTeam"){
             GameManager.instance.MyPiece.Add(this.gameObject);
         }
         Dir = (int) (gameObject.transform.rotation.z / 90) % 4;
         Attackable = false;
+        nowHp = Hp;
         
     }
     public void Update(){
-        Die();
     }
 
-    public void Hit(GameObject target, float dmg){
-        target.GetComponent<PieceController>().Hp -= dmg;
+    public void Damaged(float damage){
+        GameObject hudText = Instantiate(hudDamageText);
+        hudText.transform.position = Hudpos.position;
+        hudText.GetComponent<DamageText>().damage = damage;
+        nowHp -= damage;
+        HealthBar.GetComponent<Image>().fillAmount = nowHp/Hp;
+        Die();
     }
     public void Attack(){
         if(Attackable){
             if(GameManager.instance.Board[(int)transform.position.x + dx[Dir],(int)transform.position.z + dy[Dir]] != null  && GameManager.instance.Board[(int)transform.position.x + dx[Dir],(int)transform.position.z + dy[Dir]].tag != this.tag){
                 Debug.Log("공격!");
-                Hit(GameManager.instance.Board[(int)transform.position.x + dx[Dir],(int)transform.position.z + dy[Dir]], OffensePower);
+                GameManager.instance.Board[(int)transform.position.x + dx[Dir],(int)transform.position.z + dy[Dir]].GetComponent<PieceController>().Damaged(OffensePower);
                 Attackable = false;
             }
             
@@ -38,7 +48,7 @@ public class PieceController : MonoBehaviour
     }
 
     public void Die(){
-        if(Hp <= 0 ){
+        if(nowHp <= 0 ){
             Destroy(gameObject);
         }
     }
