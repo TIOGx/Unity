3c0bc36739939a221c521d1929257a0303669b89
JeongEunJi <wjdwl270@naver.com>
2021-08-18 19:36:07 +0900

    [Update] camera move

--- Project/BattleChessProject/Assets/Scripts/CameraManager.cs
+++ Project/BattleChessProject/Assets/Scripts/CameraManager.cs
@@ -7,6 +7,11 @@ public class CameraManager : MonoBehaviour
     public static CameraManager instance;
     public GameObject BlackTeamC;
     public GameObject WhiteTeamC;
+    private Vector3 OriginCamerapos;
+    private Quaternion OriginCamerarot;
+    private Vector3 SharedCamerapos;
+    private Quaternion SharedCamerarot;
+
     void Start()
     {
         instance = this;
@@ -21,6 +26,89 @@ public class CameraManager : MonoBehaviour
         if (Input.GetKey("2"))
         {
             WhiteTeamCameraOn();
+        }
+    }
+    IEnumerator CameraMoveCoroutine(Transform TeamOriginTransform)
+    {
+        float elapsedTime = 0;
+        float waitTime = 0.2f;
+        while (waitTime > elapsedTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            //if (GameManager.instance.GetPlayer())
+            //{
+            //    SharedCamerapos = new Vector3(3.5f, 7.05f, 3.5f);
+            //    SharedCamerarot = Quaternion.Euler(90f, 0, 0);
+            //    TeamOriginTransform.position = Vector3.Lerp(TeamOriginTransform.position, SharedCamerapos, 0.2f);
+            //    TeamOriginTransform.rotation = Quaternion.Lerp(TeamOriginTransform.rotation, SharedCamerarot, 0.2f);
+            //    yield return null;
+
+            //}
+            //else
+            //{
+            //    SharedCamerapos = new Vector3(3.5f, 7.05f, 3.5f);
+            //    SharedCamerarot = Quaternion.Euler(90f, 180f, 0);
+            //    TeamOriginTransform.position = Vector3.Lerp(TeamOriginTransform.position, SharedCamerapos, 0.2f);
+            //    TeamOriginTransform.rotation = Quaternion.Lerp(TeamOriginTransform.rotation, SharedCamerarot, 0.2f);
+            //    yield return null;
+            //}
+            TeamOriginTransform.position = Vector3.Lerp(TeamOriginTransform.position, SharedCamerapos, 0.2f);
+            TeamOriginTransform.rotation = Quaternion.Lerp(TeamOriginTransform.rotation, SharedCamerarot, 0.2f);
+            yield return null;
+        }
+        TeamOriginTransform.position = SharedCamerapos;
+        TeamOriginTransform.rotation = SharedCamerarot;
+    }
+
+
+    IEnumerator InitCamCoroutine(Transform TeamOriginTransform)
+    {
+        float elapsedTime = 0;
+        float waitTime = 0.2f;
+        while (waitTime > elapsedTime)
+        {
+            elapsedTime += Time.deltaTime;
+            TeamOriginTransform.position = Vector3.Lerp(TeamOriginTransform.position, OriginCamerapos, 0.2f);
+            TeamOriginTransform.rotation = Quaternion.Lerp(TeamOriginTransform.rotation, OriginCamerarot, 0.2f);
+            yield return null;
+        }
+        TeamOriginTransform.position = OriginCamerapos;
+        TeamOriginTransform.rotation = OriginCamerarot;
+    }
+
+
+    public void ClickPieceCamera()
+    {
+
+        if (GameManager.instance.GetPlayer())
+        {
+            OriginCamerapos = BlackTeamC.transform.position;
+            OriginCamerarot = BlackTeamC.transform.rotation;
+            SharedCamerapos = new Vector3(3.5f, 7.05f, 3.5f);
+            SharedCamerarot = Quaternion.Euler(90f, 0, 0);
+            StartCoroutine(CameraMoveCoroutine(BlackTeamC.transform));
+
+        }
+        else
+        {
+            SharedCamerapos = new Vector3(3.5f, 7.05f, 3.5f);
+            SharedCamerarot = Quaternion.Euler(90f, 180f, 0);
+            OriginCamerapos = WhiteTeamC.transform.position;
+            OriginCamerarot = WhiteTeamC.transform.rotation;
+            StartCoroutine(CameraMoveCoroutine(WhiteTeamC.transform));
+
+        }
+    }
+    public void InitailizeCamera()
+    {
+        if (GameManager.instance.GetPlayer())
+        {
+            StartCoroutine(InitCamCoroutine(BlackTeamC.transform));
+        }
+        else
+        {
+            StartCoroutine(InitCamCoroutine(WhiteTeamC.transform));
         }
     }
     public void BlackTeamCameraOn()
